8fb9aa9196ffd28c8cc829e3185661a295d9bdc9
INE Codes <abhayaps05@gmail.com>
2024-02-02 22:36:02 +0530

    Top ratings table

---
+++ IMBD_movie/Top_of_ratings.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import mysql.connector as mysql
+MyDB = mysql.connector.connect(
+    host='localhost',
+    user='root',
+    password='12345',
+    database='kaggle',
+    charset='utf8'
+)
+
+df = pd.read_csv("C:/Users/ADMIN/Documents/GitHub/Python_Projects/IMBD_movie/imdb_movie_data_2023.csv")
+df1 = list(set(df['Genre']))
+for i in range(len(df1)):
+mycur = MyDB.cursor()
