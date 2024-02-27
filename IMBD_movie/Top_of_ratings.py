f787c246b484b6e375859c5d48f15c3fd1220528
INE Codes <abhayaps05@gmail.com>
2024-02-27 07:31:35 +0530

    Distinct Genre

--- IMBD_movie/Top_of_ratings.py
+++ IMBD_movie/Top_of_ratings.py
@@ -1,17 +1,16 @@
 import pandas as pd
 import mysql.connector as mysql
-MyDB = mysql.connector.connect(
+
+MyDB = mysql.connect(
     host='localhost',
     user='root',
     password='12345',
     database='kaggle',
     charset='utf8'
 )
-
-df = pd.read_csv("C:/Users/ADMIN/Documents/GitHub/Python_Projects/IMBD_movie/imdb_movie_data_2023.csv")
-df1 = list(set(df['Genre']))
-mycur = MyDB.cursor()
-for i in range(len(df1)):
-    x = df[i,'Genre']
-    
-
+MyCursor = MyDB.cursor(buffered=True)
+MyCursor.execute("select distinct Genre from imbd_movie;")
+MyDB.commit()
+MyCursor.close()
+MyDB.close()
+print("got the genre")
